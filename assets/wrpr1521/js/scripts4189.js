a2a39dfd207cd4740ac47b998371f34634183924
CodekingOfficial <official.bellaswedis.me@gmail.com>
2024-09-08 16:08:24 +0100

    Add files via upload

---
+++ assets/wrpr1521/js/scripts4189.js
@@ -0,0 +1,21 @@
+﻿
+
+/* ====================================================
+ * LAYOUT: volunteer.php
+ * ----------------------------------------------------
+ * Version: 1
+ * Author: Steven Magallanes
+ * Summary: The Volunteer page has a toggle area for each
+ * item that is shown, along with a plus sign that converts
+ * to a minus when clicked.
+ * ====================================================*/
+$(function () {
+    $(".volunteer-item-collapse").on("show.bs.collapse", function () {
+        var m_btn = $(this).parent().find(".volunteer-collapse-btn");
+        m_btn.removeClass("fa-plus").addClass("fa-minus");
+    });
+    $(".volunteer-item-collapse").on("hide.bs.collapse", function () {
+        var m_btn = $(this).parent().find(".volunteer-collapse-btn");
+        m_btn.removeClass("fa-minus").addClass("fa-plus");
+    });
+});
